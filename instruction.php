8911280f365e413505fe6411f7839b45b717f74d
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-12-16 20:51:49 +0100

    last pushes

---
+++ instruction.php
@@ -0,0 +1,36 @@
+<?php include 'private/includes/nav.php'; ?>
+
+<div class="container">
+    <header>
+        <a href="todo.php" class="back">
+            <</a> <span class="header">Task Name</span>
+                <div class="info">
+                    i
+                </div>
+    </header>
+
+    <div class="taskimg">
+        <div class="tasktodo">
+            huts
+        </div>
+    </div>
+
+    <div class="taskaudio">
+        <span class="play">></span>
+        <span class="audio">Audio</span>
+        <span class="pause">| |</span>
+    </div>
+
+    <div class="taskwhere">
+        <div class="task_dots">
+            <div class="dot_task"></div>
+            <div class="dot_task"></div>
+            <div class="dot_task"></div>
+        </div>
+        <div class="where">
+            <span>Next ></span>
+        </div>
+    </div>
+</div>
+
+<?php include 'private/includes/footer.php'; ?>
